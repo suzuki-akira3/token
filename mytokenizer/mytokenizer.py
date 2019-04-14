57e0e6f7cb7527590f433b3fd55a254471a19f0b
suzuki-akira3 <47878916+suzuki-akira3@users.noreply.github.com>
2019-04-14 19:35:52 +0900

    under developing

--- mytokenizer/mytokenizer.py
+++ mytokenizer/mytokenizer.py
@@ -12,18 +12,22 @@ with file.open(encoding='utf-8') as f:
     doc = f.read()
 
 splitLineSpace = re.finditer(r'([^\n]+)(\n)', doc)
-textlist = []
+textList = []
 for chunk in splitLineSpace:
     dic = {'TX': chunk.group(1), 'LF':chunk.group(2)}
-    textlist.append(dic)
+    textList.append(dic)
 
-d = dotdict(dic)
-print(d.TX)
-#
-# splitspaces = re.finditer(r'([^\s]+?)(\s)', doc)
-# token_list = []
-# for ss in splitspaces:
-#     token_list.append([w for w in ss.groups()])
+# d = dotdict(dic)
+# print(d.TX)
+
+for dicText in textList:
+    dotdicText = dotdict(dicText)
+    splitspaces = re.finditer(r'([^\s]+?)(\s)',dotdicText.TX)
+    token_list = []
+    for ss in splitspaces:
+        token_list.append([w for w in ss.groups()])
+    print(token_list)
+
 #
 # for token_ in token_list:
 #     print(token_)
